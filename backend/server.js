342db430477f2fc77c83e2b36b2ea2245f971a89
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-12 11:59:03 +0530

    express router & api routes

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,21 @@
 require("dotenv").config();
 
 const express = require("express");
+//workout route
+const workoutRoutes = require("./routes/workouts");
 
 const app = express();
 
 // middleware
+// any req that comes in it looks if it has some body if it does it parses it and attaches it to the req object
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => console.log("listening on port 4000"));
