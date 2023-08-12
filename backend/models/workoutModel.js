3324299a644eda708dfb4cde939553afb06b7935
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-12 14:38:32 +0530

    Models & Schemas

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,31 @@
+//mongoose allows us to create these models and schemas for our data in the db
+// MongoDB alone is schema-less
+const mongoose = require("mongoose");
+
+// function to create new schema
+const Schema = mongoose.Schema;
+
+//Define how our workout documents should look
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// export model
+// creates Workoutes collection automatically
+// creates model
+// model is used to interact with the Workout collection bcoz it automatically creates a collection for us based on this name. It pluralizes this and build that collection in the db for us
+module.exports = mongoose.model("Workout", workoutSchema);
